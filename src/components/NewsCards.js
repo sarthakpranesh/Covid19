dbd55822d37381dd46628925162941d8616d7cbc
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-05-27 19:49:17 +0530

    Updated assets

--- src/components/NewsCards.js
+++ src/components/NewsCards.js
@@ -1,45 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-    View,
-    Text,
-    Linking,
-    Image,
-    FlatList,
-    TouchableOpacity
+  View,
+  Text,
+  Linking,
+  Image,
+  FlatList,
+  TouchableOpacity,
 } from 'react-native';
 
 // importing component
 import Precaution from './PreventionCards';
 
 class PreventionCards extends Component {
-
-    render () {
-        const { topHeadlines } = this.props;
-        return (
-            <>
-                <FlatList 
-                    data={topHeadlines}
-                    keyExtractor={(news) => news.title}
-                    renderItem={({item}) => {
-                        if (!item.title || !item.description || !item.url) {
-                            return;
-                        }
-                        return (
-                            <TouchableOpacity
-                                onPress={() => Linking.openURL(item.url)}
-                            >
-                                <Precaution 
-                                    title={item.title}
-                                    content={item.description}
-                                    src={{ uri: item.image }}
-                                />
-                            </TouchableOpacity>
-                        );
-                    }}
-                />
-            </>
-        );
-    }
+  render() {
+    const {topHeadlines} = this.props;
+    return (
+      <FlatList
+        data={topHeadlines}
+        keyExtractor={(news) => news.title}
+        renderItem={({item}) => {
+          if (!item.title || !item.description || !item.url) {
+            return;
+          }
+          return (
+            <TouchableOpacity onPress={() => Linking.openURL(item.url)}>
+              <Precaution
+                title={item.title}
+                content={item.description}
+                src={{uri: item.image}}
+              />
+            </TouchableOpacity>
+          );
+        }}
+      />
+    );
+  }
 }
 
 export default PreventionCards;
