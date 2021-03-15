e89c383fb288161edc72d3f89836fa6074c66324
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2021-03-15 23:53:31 +0530

    Minor warning fix
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- src/components/Candle.tsx
+++ src/components/Candle.tsx
@@ -1,19 +1,17 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { TouchableWithoutFeedback } from 'react-native'
-import Animated, { Easing, Extrapolate, interpolate, useAnimatedStyle, useSharedValue, withSequence, withTiming } from 'react-native-reanimated'
+import Animated from 'react-native-reanimated'
 
 export type CandleParams = {
-  key: string;
   style: any;
   onPress: () => void;
 }
 
 const Candle = (props: CandleParams) => {
-  const { key, style, onPress } = props
+  const { style, onPress } = props
 
   return (
     <TouchableWithoutFeedback
-      key={key}
       onPress={onPress}
     >
       <Animated.View
