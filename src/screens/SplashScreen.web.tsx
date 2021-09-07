8d9a1a3455a7d8de6f0c8e626361bc490a62d793
sarthakpranesh <sarthakpranesh08@gmail.com>
2021-09-07 16:59:51 +0530

    Added eslintignore and performed linting
    Signed-off-by: sarthakpranesh <sarthakpranesh08@gmail.com>

--- src/screens/SplashScreen.web.tsx
+++ src/screens/SplashScreen.web.tsx
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#DEF7FF"
+    backgroundColor: "#DEF7FF",
   },
   icon: {
     width: 200,
